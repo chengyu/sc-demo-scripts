39e11a2d2c377a896b1daf666e84506df3f6d7a5
Chengyu <chengy.fan@gmail.com>
2018-11-04 00:31:40 -0400

    add scripts for run demo

---
+++ run_ndn_services_genom_test.sh
@@ -0,0 +1,18 @@
+ARG=$1
+if [ $ARG == "start" ]; then
+    timestamp=`date "+%Y-%m-%d-%H:%M:%S"`
+    /usr/local/bin/nfd-start &>/tmp/nfd_$timestamp.log
+#    sleep 3
+#    /usr/local/bin/nfdstat_c -p /cmip5/den/ndnmap/stats -d 1 &
+#    # using nlsr
+#    /usr/local/bin/nfdc set-strategy /catalog/genom /localhost/nfd/strategy/weighted-load-balancer
+
+    sleep 3
+    /usr/local/bin/atmos-catalog -f /home/chengyu/workspace/sc-demo/catalog-conf/genom-test.conf &
+
+    nohup /usr/local/bin/ndn-repo-ng &
+fi
+
+if [ $ARG == "stop" ]; then
+    /usr/local/bin/nfd-stop
+fi
